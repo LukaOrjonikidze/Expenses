6de11a9108100e700eeb850702567d216472c0e6
LukaOrjonikidze <lukaorjonikidze24@gmail.com>
2023-02-22 02:17:59 +0400

    added expense item and date components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import ExpenseItem from "./components/ExpenseItem";
+import expenses from "./expenses";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      {expenses.map((expense) => (
+        <ExpenseItem 
+        id={expense.id} 
+        key={expense.id} 
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+        />
+      ))}
     </div>
   );
 }
