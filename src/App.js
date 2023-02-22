12cd2c82c4a0a2caf8e08f12b5d66300525b7a06
LukaOrjonikidze <lukaorjonikidze24@gmail.com>
2023-02-22 15:19:07 +0400

    Added expense input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import expenses from "./expenses";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses expenses={expenses}/>
     </div>
   );
